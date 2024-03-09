5b5e3bb3cf8ccaef5913cd247c779154100ca551
BlessKobby <amablebless@gmail.com>
2024-03-09 05:43:25 -0800

    AirBnB - Deploy static.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,63 @@
 #!/usr/bin/python3
-"""A script that compresses web static package.
-"""
-from fabric.api import *
+from fabric.api import local, env, put, run
+from os.path import exists
 from datetime import datetime
-from os import path
-
 
 env.hosts = ['34.204.81.159', '52.73.255.61']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.user = 'ubuntu'  # Update with your SSH username
+env.key_filename = '~/.ssh/school'  # Update with the path to your SSH private key
 
+def do_pack():
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/".format(filename))
+        filepath = "versions/web_static_{}.tgz".format(filename)
+        filesize = os.path.getsize(filepath)
+        print("web_static packed: {} -> {} Bytes".format(filepath, filesize))
+
+        return filepath
+    except Exception as e:
+        return None
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
+    """Deploy the archive to web servers"""
 
-                # upload archive
-                put(archive_path, '/tmp/')
+    if not exists(archive_path):
+        return False
 
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
 
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
+        # Extract the archive to /data/web_static/releases/<archive filename without extension>
+        archive_filename = archive_path.split('/')[-1].split('.')[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(archive_filename))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_path.split('/')[-1], archive_filename))
 
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(archive_path.split('/')[-1]))
 
-                # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+        # Move files to the proper location
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
+            .format(archive_filename, archive_filename))
 
-                # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                    .format(timestamp))
+        # Remove unnecessary directory
+        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_filename))
 
-                # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
+        # Delete the symbolic link /data/web_static/current
+        run("rm -rf /data/web_static/current")
 
-                # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
+        # Create a new symbolic link /data/web_static/current
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive_filename))
 
-        # return True on success
+        print("New version deployed!")
+
         return True
+    except Exception as e:
+        return False
+
